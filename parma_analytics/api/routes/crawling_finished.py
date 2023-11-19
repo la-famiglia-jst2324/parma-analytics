5b3e88ebe8b4d70873e207bad9b36cc5f864d9df
alouiii <64066060+alouiii@users.noreply.github.com>
2023-11-19 23:12:47 +0100

    feat: Add REST API for data crawling is finished (#8)
    Co-authored-by: root <root@DESKTOP-C4HEHQH>

---
+++ parma_analytics/api/routes/crawling_finished.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+
+
+router = APIRouter()
+
+
+@router.post("/crawling-finished", status_code=201)
+def crawling_finished():
+    ## Later specify the trigger flow here
+
+    # Return a JSON response
+    return jsonable_encoder({"message": "Notified about crawling finished"})
