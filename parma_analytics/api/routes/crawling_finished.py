49fd3525ff95c51d39f2c52fcfa759f284c19839
alouiii <achref.aloui73@gmail.com>
2023-11-22 23:48:20 +0100

    implements review comments

--- parma_analytics/api/routes/crawling_finished.py
+++ parma_analytics/api/routes/crawling_finished.py
@@ -9,7 +9,11 @@ from parma_analytics.api.models.crawling_finished import (
 router = APIRouter()
 
 
-@router.post("/crawling-finished", status_code=201)
+@router.post(
+    "/crawling-finished",
+    status_code=201,
+    description="Endpoint to receive notifications when all crawling jobs have completed. This allows the system to proceed with data processing once all data has been gathered.",
+)
 def crawling_finished(done_message: ApiCrawlingFinishedCreateIn):
     ## Later specify the trigger flow here
     print(done_message.incoming_message)
