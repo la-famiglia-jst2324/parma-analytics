5cfb143c6372600b41f2ba21208a902ed1626c09
alouiii <achref.aloui73@gmail.com>
2023-11-22 17:35:54 +0100

    change http client

---
+++ parma_analytics/api/models/new_company.py
@@ -0,0 +1,37 @@
+"""Example of a model for the API."""
+
+from pydantic import BaseModel
+
+# ------------------------------------------------------------------------------------ #
+#                                       Internal                                       #
+# ------------------------------------------------------------------------------------ #
+
+
+class _ApiNewCompanyBase(BaseModel):
+    """Internal base model for the new company endpoints."""
+
+    company_name: str
+
+
+class _ApiNewCompanyOutBase(_ApiNewCompanyBase):
+    """Output base model for the several endpoint."""
+
+    return_message: str
+
+
+# ------------------------------------------------------------------------------------ #
+#                                     Create Models                                    #
+# ------------------------------------------------------------------------------------ #
+
+
+class ApiNewCompanyCreateIn(_ApiNewCompanyBase):
+    """Input model for the NewCompany creation endpoint."""
+
+    description: str
+    added_by: str
+
+
+class ApiNewCompanyCreateOut(_ApiNewCompanyOutBase):
+    """Output model for the NewCompany creation endpoint."""
+
+    pass
--- parma_analytics/api/routes/new_company.py
+++ parma_analytics/api/routes/new_company.py
@@ -1,37 +1,31 @@
-from fastapi import APIRouter, HTTPException, Body
-from fastapi.encoders import jsonable_encoder
-import httpx
-from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException
+import requests
+
+
+from parma_analytics.api.models.new_company import (
+    ApiNewCompanyCreateIn,
+    ApiNewCompanyCreateOut,
+)
 
 router = APIRouter()
 
 
-# To be extended when needed.
-class Company(BaseModel):
-    id: int
-    name: str
-    description: str
-    added_by: str
-
-
 @router.post("/new-company", status_code=201)
-async def register_new_company(company: Company = Body(..., embed=True)):
+def register_new_company(company: ApiNewCompanyCreateIn):
     # Validate or process the company data as needed
 
     # Forward the company data to another backend
     try:
-        async with httpx.AsyncClient() as client:
-            # TODO: change the sourcing url once provided.
-            response = await client.post(
-                "http://sourcing-backend/api/receive-company", json=company.model_dump()
-            )
-            response.raise_for_status()
-    except httpx.HTTPError as e:
+        # TODO: change the sourcing url once provided.
+        response = requests.post("http://sourcing-backend/api/receive-company", company)
+        response.raise_for_status()
+    except requests.HTTPError as e:
         raise HTTPException(
             status_code=500,
             detail=f"Error forwarding company to the data sourcing modules: {str(e)}",
         )
 
-    return jsonable_encoder(
-        {"message": "New company forwarded to data sourcing successfully"}
+    return ApiNewCompanyCreateOut(
+        company_name=company.company_name,
+        return_message="New company forwarded to data sourcing successfully",
     )
