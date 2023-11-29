b6f21fce2e36d7684b03398a83fe6a913934cccc
alouiii <achref.aloui73@gmail.com>
2023-11-29 18:41:21 +0100

    merge main to fe

--- parma_analytics/db/prod/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Models for the database models using sqlalchemy."""
-
-from .base import DbBase
-from .dummy import DbDummy
-from .source_measurement_db import DbSourceMeasurement
-
-__all__ = ["DbBase", "DbDummy", "DbSourceMeasurement"]
-
