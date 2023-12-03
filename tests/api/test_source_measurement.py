142b8847acf78aa8f5c9bc541b111da16d5ffc9a
alouiii <achref.aloui73@gmail.com>
2023-12-03 15:01:30 +0100

    measurement service test chagnes

--- tests/api/test_source_measurement.py
+++ tests/api/test_source_measurement.py
@@ -14,6 +14,7 @@ def client():
 # Mock the get_db dependency
 def mock_get_db():
     db = MagicMock()
+    db.execute.return_value = MagicMock() #redundant
     return db
 
 
@@ -36,13 +37,13 @@ def test_create_source_measurement(mock_bll, mock_get_db, client):
     mock_get_db.assert_called_once()
     mock_bll.assert_called_once()
 
-
-@patch("parma_analytics.db.prod.engine.get_db", new_callable=mock_get_db)
 @patch("parma_analytics.bl.source_measurement_bll.list_source_measurements_bll")
+@patch("parma_analytics.db.prod.engine.py", new_callable=mock_get_db)
 def test_read_all_source_measurements(mock_bll, mock_get_db, client):
     mock_bll.return_value = []  # Mock the return value of the business logic function
     response = client.get("/source-measurement")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+    assert response.content == []
     mock_get_db.assert_called_once()
     mock_bll.assert_called_once()
