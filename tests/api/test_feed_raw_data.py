cbd1cb378c66276288b7cf7a59065c72c67d5095
mfteloglu <mfteloglu@gmail.com>
2023-12-07 23:02:11 +0100

    test: add unit test

---
+++ tests/api/test_feed_raw_data.py
@@ -0,0 +1,19 @@
+import pytest
+from parma_analytics.api import app
+from fastapi.testclient import TestClient
+from starlette import status
+
+
+@pytest.fixture
+def client():
+    assert app
+    return TestClient(app)
+
+
+def test_feed_raw_data(client):
+    test_data = {"source_name": "Linkedin", "raw_data": "Test JSON decoded data"}
+    response = client.post("/feed-raw-data", json=test_data)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()["source_name"] == "Linkedin"
+    assert response.json()["raw_data"] == "Test JSON decoded data"
