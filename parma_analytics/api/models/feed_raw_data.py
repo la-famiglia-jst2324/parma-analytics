ef15c78b5c863e154b8e413e3b90d4347058081d
Mehmet Fatih Teloğlu <43525350+mfteloglu@users.noreply.github.com>
2023-12-09 00:20:29 +0100

    feat: Implement the endpoint for modules to feed the raw data (#30)

---
+++ parma_analytics/api/models/feed_raw_data.py
@@ -0,0 +1,40 @@
+"""Example of a model for the API."""
+
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Any
+
+# ------------------------------------------------------------------------------------ #
+#                                       Internal                                       #
+# ------------------------------------------------------------------------------------ #
+
+
+class _ApiFeedRawDataBase(BaseModel):
+    """Internal base model for the raw data endpoints."""
+
+    source_name: str
+    raw_data: dict[str, Any]
+
+
+class _ApiFeedRawDataOutBase(_ApiFeedRawDataBase):
+    """Output base model for the several endpoint."""
+
+    timestamp: datetime
+    return_message: str
+
+
+# ------------------------------------------------------------------------------------ #
+#                                     Create Models                                    #
+# ------------------------------------------------------------------------------------ #
+
+
+class ApiFeedRawDataCreateIn(_ApiFeedRawDataBase):
+    """Input model for the raw data creation endpoint."""
+
+    pass
+
+
+class ApiFeedRawDataCreateOut(_ApiFeedRawDataOutBase):
+    """Output model for the raw data creation endpoint."""
+
+    pass
