d81ae6731a6b86ba4a7d16229a619acbe100c86f
Deepankar Ratra <drattra@gmail.com>
2023-12-13 05:53:34 +0100

    Removing Unused Files

--- parma_analytics/reporting/generate_pdf.py
+++
@@ -1,7 +0,0 @@
-from matplotlib.backends.backend_pdf import PdfPages
-
-
-def export_to_pdf(plt, file_name):
-    with PdfPages(file_name) as pdf:
-        pdf.savefig()  # save figure to pdf
-        plt.close()
