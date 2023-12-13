ba715dbe04ea9d50e704e0d8a6c8f5793c150b7f
onur-oruc <honouroruc@gmail.com>
2023-12-13 02:20:38 +0100

    refactor: change enums to literals

--- parma_analytics/reporting/notification_service_manager.py
+++ parma_analytics/reporting/notification_service_manager.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Literal
 from .db_operations import (
     fetch_user_ids_for_company,
     fetch_channel_ids,
@@ -8,19 +8,9 @@ from .db_operations import (
 from enum import Enum
 
 
-class Category(Enum):
-    COMPANY = "COMPANY"
-    BUCKET = "BUCKET"
-
-
-class MessageType(Enum):
-    NOTIFICATION = "NOTIFICATION"
-    REPORT = "REPORT"
-
-
-class ServiceType(Enum):
-    EMAIL = "EMAIL"
-    SLACK = "SLACK"
+Category = Literal["COMPANY", "BUCKET"]
+MessageType = Literal["NOTIFICATION", "REPORT"]
+ServiceType = Literal["EMAIL", "SLACK"]
 
 
 class NotificationServiceManager:
@@ -41,10 +31,10 @@ class NotificationServiceManager:
     def get_notification_destinations(self) -> List[str]:
         company_id = self.company_or_bucket_id
         # if the category is a bucket, get a company_id from the bucket. Assume that every company in the bucket is subscribed by the user.
-        if self.category == Category.BUCKET:
+        if self.category == "BUCKET":
             company_id = fetch_company_id_from_bucket(self.company_or_bucket_id)
         user_ids = fetch_user_ids_for_company(company_id)
         channel_ids = fetch_channel_ids(user_ids, self.subscription_table)
         return fetch_notification_destinations(
-            channel_ids, self.entity_type.value, self.service_type.value
+            channel_ids, self.entity_type, self.service_type
         )
