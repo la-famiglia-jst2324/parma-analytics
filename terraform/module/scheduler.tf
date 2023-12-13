6325dc8da65fc2c0a622651d824e50e9fd24e7ca
Robin Holzinger <robin.holzinger@tum.de>
2023-12-13 16:52:55 +0100

    adjust schedule to hourly

--- terraform/module/scheduler.tf
+++ terraform/module/scheduler.tf
@@ -1,6 +1,6 @@
 resource "google_cloud_scheduler_job" "schedule_job" {
   name     = "schedule-job-${var.env}"
-  schedule = "0 12 * * *"  # Cron Expression for Every Day at Midday
+  schedule = "0 * * * *"  # Every full hour
 
   http_target {
     uri         = "https://analytics.${var.api_subdomain}${var.base_domain}/schedule"
