d2a6e3b75802a8a13a5c3371d450e482c2d6bf48
Mehmet Fatih Teloğlu <43525350+mfteloglu@users.noreply.github.com>
2023-12-13 16:00:51 +0100

    feat: Implement mapping schema CRUD and add normalization (#38)
    Co-authored-by: Robin Holzinger <robin.holzinger@tum.de>

--- parma_analytics/api/routes/feed_raw_data.py
+++ parma_analytics/api/routes/feed_raw_data.py
@@ -7,8 +7,12 @@ from parma_analytics.api.models.feed_raw_data import (
     ApiFeedRawDataCreateIn,
     ApiFeedRawDataCreateOut,
 )
-from parma_analytics.db.mining.models import RawDataIn
-from parma_analytics.db.mining.service import store_raw_data
+from parma_analytics.db.mining.models import NormalizationSchema, RawData, RawDataIn
+from parma_analytics.db.mining.service import (
+    read_normalization_schema_by_datasource,
+    store_raw_data,
+)
+from parma_analytics.sourcing.normalization.normalization_engine import normalize_data
 
 router = APIRouter()
 
@@ -28,11 +32,38 @@ def feed_raw_data(body: ApiFeedRawDataCreateIn) -> ApiFeedRawDataCreateOut:
             data=body.raw_data,
         ),
     )
+    timestamp = datetime.now()
+    return_message = "Raw data received and saved. "
+
+    mapping_schemas = read_normalization_schema_by_datasource(body.source_name)
+
+    if not mapping_schemas or len(mapping_schemas) == 0:
+        return_message += "However normalization schema cannot be found"
+        latest_mapping_schema = NormalizationSchema(schema=None)
+    else:
+        # Get the latest mapping schema for now
+        latest_mapping_schema = mapping_schemas[-1]
+
+    _ = normalize_data(
+        raw_data=RawData(
+            mining_trigger="",
+            status="success",
+            company_id=body.company_id,
+            data=body.raw_data,
+            create_time=timestamp,
+            id="",
+            update_time=None,
+            read_time=None,
+        ),
+        mapping_schema=latest_mapping_schema,
+    )
+
+    # TODO: Write normalized_data(_) to the PROD DB
 
     return ApiFeedRawDataCreateOut(
-        return_message="Raw data received and saved",
+        return_message=return_message,
         source_name=body.source_name,
-        timestamp=datetime.now(),
+        timestamp=timestamp,
         document_id=saved_document.id,
         company_id=body.company_id,
         raw_data=body.raw_data,
--- parma_analytics/db/mining/service.py
+++ parma_analytics/db/mining/service.py
@@ -7,11 +7,17 @@ from firebase_admin.firestore import firestore as firestore_types
 from parma_analytics.db.mining.engine import get_engine
 from parma_analytics.db.mining.internal.models import DocTemplateInstance
 from parma_analytics.db.mining.internal.storage import (
+    _resolve_from_path,
     filter_documents_from_path,
     read_document_from_path,
     save_document_from_template,
 )
-from parma_analytics.db.mining.models import RawData, RawDataIn
+from parma_analytics.db.mining.models import (
+    NormalizationSchema,
+    NormalizationSchemaIn,
+    RawData,
+    RawDataIn,
+)
 from parma_analytics.utils.uuid import generate_uuid
 
 
@@ -82,3 +88,75 @@ def read_raw_data_by_company(datasource: str, company_id: str) -> list[dict[str,
         response.append(read_raw_data_by_id(datasource, filtered_document.id))
 
     return response
+
+
+def store_normalization_schema(
+    datasource: str, normalization_schema: NormalizationSchemaIn
+) -> firestore_types.DocumentReference:
+    """Store normalization schema in the database.
+
+    Args:
+        datasource: The datasource name.
+        normalization_schema: The normalization schema.
+    """
+    engine = get_engine()
+    instance_id = generate_uuid()
+    instance = DocTemplateInstance(
+        name=instance_id,
+        values=dict(normalization_schema),
+    )
+    return save_document_from_template(
+        engine,
+        f"parma/mining/datasource/{datasource}/normalization_schema/{instance_id}",
+        instance,
+    )
+
+
+def read_normalization_schema_by_id(
+    datasource: str, instance_id: str
+) -> NormalizationSchema:
+    """Read normalization schema from the database.
+
+    Args:
+        datasource: The datasource name.
+        instance_id: The instance id.
+
+    Returns:
+        The normalization schema.
+    """
+    snapshot = read_document_from_path(
+        get_engine(),
+        f"parma/mining/datasource/{datasource}/normalization_schema/{instance_id}",
+    )
+    return NormalizationSchema(
+        id=snapshot.id,
+        create_time=snapshot.create_time,
+        update_time=snapshot.update_time,
+        read_time=snapshot.read_time,
+        schema=snapshot.get("schema"),
+    )
+
+
+def read_normalization_schema_by_datasource(
+    datasource: str,
+) -> list[NormalizationSchema]:
+    """Read all normalization_schema for a company from the database.
+
+    Args:
+        datasource: The datasource name.
+
+    Returns:
+        List of normalization schema.
+    """
+    current_collection = _resolve_from_path(
+        get_engine(),
+        f"parma/mining/datasource/{datasource}/normalization_schema",
+        False,
+    )
+    response = []
+    for normalization_schema in current_collection.get():
+        response.append(
+            read_normalization_schema_by_id(datasource, normalization_schema.id)
+        )
+
+    return sorted(response, key=lambda x: x.create_time)
