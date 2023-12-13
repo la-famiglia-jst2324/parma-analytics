51b08c1cfb4fc28d89c4e87ed3743881034b88e2
Robin Holzinger <robin.holzinger@tum.de>
2023-12-13 12:43:31 +0100

    ci: Add API keys to deployments (#39)

--- terraform/module/variables.tf
+++ terraform/module/variables.tf
@@ -36,3 +36,32 @@ variable "api_subdomain" {
   type        = string
   sensitive   = false
 }
+
+/* ------------------------------------ SendGrid ------------------------------------ */
+
+variable "sendgrid_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "sendgrid_from_email" {
+  type      = string
+  sensitive = false
+}
+
+variable "sendgrid_notification_template_id" {
+  type      = string
+  sensitive = false
+}
+
+variable "sendgrid_report_template_id" {
+  type      = string
+  sensitive = false
+}
+
+/* ------------------------------------- ChatGpt ------------------------------------ */
+
+variable "chatgpt_api_key" {
+  type      = string
+  sensitive = true
+}
