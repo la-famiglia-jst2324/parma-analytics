6caf7b5ecae30dc5827ef8eed780d52efb644277
Robin Holzinger <robin.holzinger@tum.de>
2023-12-15 16:00:46 +0100

    ci: Reduce deployment image size (#52)

--- terraform/module/scheduler.tf
+++ terraform/module/scheduler.tf
@@ -1,6 +1,7 @@
 resource "google_cloud_scheduler_job" "schedule_job" {
-  name     = "schedule-job-${var.env}"
-  schedule = "0 * * * *"  # Every full hour
+  depends_on = []
+  name       = "schedule-job-${var.env}"
+  schedule   = "0,30 * * * *" # Every full and half hour
 
   http_target {
     uri         = "https://analytics.${var.api_subdomain}${var.base_domain}/schedule"
