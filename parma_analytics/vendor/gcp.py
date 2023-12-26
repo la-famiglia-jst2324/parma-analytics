f95e6d7796b56319880894100c6f86b27ccbbd2f
Robin Holzinger <robin.holzinger@tum.de>
2023-12-27 00:40:29 +0100

    Fix env/file

--- parma_analytics/vendor/gcp.py
+++ parma_analytics/vendor/gcp.py
@@ -18,13 +18,18 @@ CREDENTIALS_PATH = SECRETS_DIR / "la-famiglia-parma-ai-secret-manager.json"
 def get_credentials() -> service_account.Credentials:
     """Get credentials for Google Cloud Platform."""
     env_var = os.environ.get("GCP_SECRET_MANAGER_CERTIFICATE")
-    if not env_var and not CREDENTIALS_PATH.exists():
+    if env_var:
+        credentials = service_account.Credentials.from_service_account_info(
+            json.loads(env_var)
+        )
+    elif CREDENTIALS_PATH.exists():
+        credentials = service_account.Credentials.from_service_account_file(
+            CREDENTIALS_PATH
+        )
+    else:
         raise FileNotFoundError(
             f"Credentials file not found at {CREDENTIALS_PATH}. "
             "Please set the environment variable GCP_SECRET_MANAGER_CERTIFICATE "
             "to the JSON string of the credentials."
         )
-    credentials = service_account.Credentials.from_service_account_file(
-        json.loads(env_var) if env_var else CREDENTIALS_PATH
-    )
     return credentials
