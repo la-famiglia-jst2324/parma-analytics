6813c506a65b2a1f56840a0a4ba9da5ae99b47ba
achref <achref.aloui73@gmail.com>
2023-12-31 12:45:44 +0100

    add the changes

--- parma_analytics/bl/register_measurement_values.py
+++ parma_analytics/bl/register_measurement_values.py
@@ -1,8 +1,8 @@
-"""This module contains the functions for registering the measurement values."""
+"""This module contains the functions for registering measurement values."""
 
 import logging
-from collections.abc import Callable
 from datetime import datetime
+from typing import Any
 
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
@@ -12,20 +12,17 @@ from parma_analytics.db.prod.company_source_measurement_query import (
     get_by_company_and_measurement_ids_query,
 )
 from parma_analytics.db.prod.engine import get_session
-from parma_analytics.db.prod.measurement_comment_value_query import (
-    create_measurement_comment_value_query,
-)
-from parma_analytics.db.prod.measurement_float_value_query import (
-    create_measurement_float_value_query,
-)
-from parma_analytics.db.prod.measurement_int_value_query import (
-    create_measurement_int_value_query,
-)
-from parma_analytics.db.prod.measurement_paragraph_value_query import (
-    create_measurement_paragraph_value_query,
-)
-from parma_analytics.db.prod.measurement_text_value_query import (
-    create_measurement_text_value_query,
+from parma_analytics.db.prod.measurement_value_query import MeasurementValueCRUD
+from parma_analytics.db.prod.models.measurement_value_models import (
+    MeasurementCommentValue,
+    MeasurementDateValue,
+    MeasurementFloatValue,
+    MeasurementImageValue,
+    MeasurementIntValue,
+    MeasurementLinkValue,
+    MeasurementNestedValue,
+    MeasurementParagraphValue,
+    MeasurementTextValue,
 )
 from parma_analytics.sourcing.normalization.normalization_model import NormalizedData
 
@@ -60,28 +57,14 @@ def register_values(normalized_measurement: NormalizedData) -> int:
                     },
                 )
 
-            # Map the types to their corresponding functions
-            type_functions: dict[str, Callable] = {
-                "int": handle_int,
-                "float": handle_float,
-                "paragraph": handle_paragraph,
-                "text": handle_text,
-                "comment": handle_comment,
-            }
-
-            # Get the function for the type of the source_measurement
-            handle_type = type_functions.get(measurement_type)
-
             # Call the function
-            if handle_type is not None:
-                created_measurement_id = handle_type(
-                    session,
-                    value,
-                    timestamp,
-                    company_measurement.company_measurement_id,
-                )
-            else:
-                raise ValueError(f"Invalid type: {measurement_type}")
+            created_measurement_id = handle_value(
+                session,
+                measurement_type,
+                value,
+                timestamp,
+                company_measurement.company_measurement_id,
+            )
 
             return created_measurement_id
 
@@ -93,14 +76,19 @@ def register_values(normalized_measurement: NormalizedData) -> int:
         return -1
 
 
-# Define the functions for each type
-def handle_int(
-    session: Session, value: int, timestamp: datetime, company_measurement_id: int
+# Determines and calls the create measurement for each measurement type
+def handle_value(
+    session: Session,
+    measurement_type: str,
+    value: Any,
+    timestamp: datetime,
+    company_measurement_id: int,
 ) -> int:
-    """Registers an int value and returns the id.
+    """Registers a value of a specific type and returns the id.
 
     Args:
         session: The database session.
+        measurement_type: The type of the measurement to be registered.
         value: The value to be registered.
         timestamp: The timestamp when the value was registered.
         company_measurement_id: The ID of the company measurement.
@@ -108,107 +96,30 @@ def handle_int(
     Returns:
         The created measurement value id.
     """
-    return create_measurement_int_value_query(
-        session,
-        {
-            "value": value,
-            "timestamp": timestamp,
-            "company_measurement_id": company_measurement_id,
-        },
-    )
+    query_functions = {
+        "int": MeasurementValueCRUD(MeasurementIntValue).create_measurement_value,
+        "float": MeasurementValueCRUD(MeasurementFloatValue).create_measurement_value,
+        "paragraph": MeasurementValueCRUD(
+            MeasurementParagraphValue
+        ).create_measurement_value,
+        "text": MeasurementValueCRUD(MeasurementTextValue).create_measurement_value,
+        "comment": MeasurementValueCRUD(
+            MeasurementCommentValue
+        ).create_measurement_value,
+        "link": MeasurementValueCRUD(MeasurementLinkValue).create_measurement_value,
+        "image": MeasurementValueCRUD(MeasurementImageValue).create_measurement_value,
+        "date": MeasurementValueCRUD(MeasurementDateValue).create_measurement_value,
+        "nested": MeasurementValueCRUD(MeasurementNestedValue).create_measurement_value,
+    }
 
-
-def handle_float(
-    session: Session, value: float, timestamp: datetime, company_measurement_id: int
-) -> int:
-    """Registers a float value and returns the id.
-
-    Args:
-        session: The database session.
-        value: The value to be inserted into the database.
-        timestamp: The timestamp when the value was registered.
-        company_measurement_id: The ID of the company measurement.
-
-    Returns:
-        The created measurement value id.
-    """
-    return create_measurement_float_value_query(
-        session,
-        {
-            "value": value,
-            "timestamp": timestamp,
-            "company_measurement_id": company_measurement_id,
-        },
-    )
-
-
-def handle_paragraph(
-    session: Session, value: str, timestamp: datetime, company_measurement_id: int
-) -> int:
-    """Registers a paragraph value and returns the id.
-
-    Args:
-        session: The database session.
-        value: The value to be registered.
-        timestamp: The timestamp when the value was registered.
-        company_measurement_id: The ID of the company measurement.
-
-    Returns:
-        The created measurement value id.
-    """
-    return create_measurement_paragraph_value_query(
-        session,
-        {
-            "value": value,
-            "timestamp": timestamp,
-            "company_measurement_id": company_measurement_id,
-        },
-    )
-
-
-def handle_text(
-    session: Session, value: str, timestamp: datetime, company_measurement_id: int
-) -> int:
-    """Registers a text value and returns the id.
-
-    Args:
-        session: The database session.
-        value: The value to be registered.
-        timestamp: The timestamp when the value was registered.
-        company_measurement_id: The ID of the company measurement.
-
-    Returns:
-        The created measurement value id.
-    """
-    return create_measurement_text_value_query(
-        session,
-        {
-            "value": value,
-            "timestamp": timestamp,
-            "company_measurement_id": company_measurement_id,
-        },
-    )
-
-
-def handle_comment(
-    session: Session, value: str, timestamp: datetime, company_measurement_id: int
-) -> int:
-    """Registers a comment value and returns the id.
-
-    Args:
-        session: The database session.
-        value: The value to be registered.
-        timestamp: The timestamp when the value was registered.
-        company_measurement_id: The ID of the company measurement.
-
-    Returns:
-        The created measurement value id.
-    """
-    return create_measurement_comment_value_query(
-        session,
-        {
-            "value": value,
-            "timestamp": timestamp,
-            "company_measurement_id": company_measurement_id,
-        },
-    )
+    if measurement_type in query_functions:
+        return query_functions[measurement_type](
+            session,
+            {
+                "value": value,
+                "timestamp": timestamp,
+                "company_measurement_id": company_measurement_id,
+            },
+        )
+    else:
+        raise ValueError(f"Invalid measurement type: {measurement_type}")
