cbe7304ef81adaa8c0e68b99697ccfda02cdbda1
Ege Kocabas <kocabas.ege@tum.de>
2024-01-10 22:03:57 +0100

    update tests

--- tests/api/test_crawling_finished.py
+++ tests/api/test_crawling_finished.py
@@ -1,10 +1,12 @@
 import logging
+from unittest.mock import patch
 
 import pytest
 from fastapi.testclient import TestClient
 from starlette import status
 
 from parma_analytics.api import app
+from parma_analytics.bl.mining_module_manager import MiningModuleManager
 
 logger = logging.getLogger(__name__)
 
@@ -22,8 +24,12 @@ def test_crawling_finished(client):
         "errors": None,
     }
 
-    response = client.post("/crawling-finished", json=test_data)
+    with patch.object(
+        MiningModuleManager, "set_task_status_success_with_id"
+    ) as mock_method:
+        response = client.post("/crawling-finished", json=test_data)
 
+    assert mock_method.called
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json() == {
         "task_id": test_data["task_id"],
@@ -33,22 +39,42 @@ def test_crawling_finished(client):
 
 
 def test_crawling_finished_with_errors(client):
+    errors = {
+        "error1": {"error_type": "Type1", "error_description": "Description1"},
+        "error2": {"error_type": "Type2", "error_description": "Description2"},
+    }
     test_data = {
         "task_id": 12345,
-        "errors": {
-            "error1": {"error_type": "Type1", "error_description": "Description1"},
-            "error2": {"error_type": "Type2", "error_description": "Description2"},
-        },
+        "errors": errors,
     }
 
-    response = client.post("/crawling-finished", json=test_data)
+    with patch.object(
+        MiningModuleManager, "set_task_status_success_with_id"
+    ) as mock_method:
+        response = client.post("/crawling-finished", json=test_data)
 
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json() == {
-        "task_id": test_data["task_id"],
-        "errors": test_data["errors"],
-        "return_message": "Notified about crawling finished",
+    assert mock_method.called
+    response_json = response.json()
+    assert response_json["task_id"] == test_data["task_id"]
+    assert response_json["errors"] == errors
+    assert response_json["return_message"] == "Notified about crawling finished"
+
+
+def test_crawling_finished_exception_handling(client):
+    test_data = {
+        "task_id": 12345,
+        "errors": None,
     }
+
+    with patch.object(
+        MiningModuleManager,
+        "set_task_status_success_with_id",
+        side_effect=Exception("Test Exception"),
+    ):
+        response = client.post("/crawling-finished", json=test_data)
+
+    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert "Test Exception" in response.json().get("detail", "")
 
 
 def test_crawling_finished_missing_field(client):
