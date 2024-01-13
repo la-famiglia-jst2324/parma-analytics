233c0530800ff3c9051d4ef4c01d3e13a9de7a3e
Robin Holzinger <robin.holzinger@tum.de>
2024-01-13 17:58:19 +0100

    fix

--- tests/vendor/test_gcp.py
+++ tests/vendor/test_gcp.py
@@ -30,7 +30,7 @@ def test_get_credentials(tmp_path_factory, monkeypatch):
     ):
         if not CREDENTIALS_PATH.exists():
             tmp_path = tmp_path_factory.mktemp(
-                f"tmpfile-gcp-secrets-{generate_uuid()}.json"
+                f"tmpfile-gcp-secrets-{generate_uuid()}.json", numbered=False
             )
             tmp_path.write_text(gcp_secret)
             monkeypatch.setattr(
