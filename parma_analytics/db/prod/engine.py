6a4b329d3cd3da0c31ab0cfd391cf6d83e2db3ce
Robin Holzinger <robin.holzinger@tum.de>
2024-01-17 21:00:53 +0100

    Adjust sessionmaker

--- parma_analytics/db/prod/engine.py
+++ parma_analytics/db/prod/engine.py
@@ -34,7 +34,7 @@ def get_session() -> Iterator[Session]:
     Yields:
         A database session.
     """
-    db = sessionmaker(autocommit=False, autoflush=False, engine=True)()
+    db = sessionmaker(engine=get_engine(), autocommit=False, autoflush=False)()
     try:
         yield db
     except:
