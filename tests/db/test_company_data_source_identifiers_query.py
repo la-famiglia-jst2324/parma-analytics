45831b2a6cdca24b82ea4aa4ed0450c2e945b95d
achref <achref.aloui73@gmail.com>
2024-01-20 23:43:23 +0100

    add company data source query and bll

--- tests/db/test_company_data_source_identifiers_query.py
+++ tests/db/test_company_data_source_identifiers_query.py
@@ -1,23 +1,18 @@
-from datetime import datetime
 from unittest.mock import MagicMock, patch
 
 import pytest
 
-from parma_analytics.bl.company_data_source_identifiers_bll import (
-    IdentifierData,
-    IdentifierUpdateData,
-)
-from parma_analytics.db.prod.company_data_source_identifiers_query import (
-    create_company_data_source_identifier,
-    delete_company_data_source_identifier,
-    get_company_data_source_identifiers,
-    update_company_data_source_identifier,
+from parma_analytics.db.prod.company_data_source_query import (
+    CompanyDataSourceData,
+    CompanyDataSourceUpdateData,
+    create_company_data_source,
+    delete_company_data_source,
+    get_all_company_data_sources,
+    get_all_company_data_sources_by_data_source_id,
+    get_company_data_source,
+    update_company_data_source,
 )
 from parma_analytics.db.prod.models.company_data_source import CompanyDataSource
-from parma_analytics.db.prod.models.company_data_source_identifier import (
-    CompanyDataSourceIdentifier,
-    IdentifierType,
-)
 
 
 @pytest.fixture
@@ -26,59 +21,56 @@ def mock_db():
 
 
 @pytest.fixture
-def mock_company_data_source_identifier():
-    return MagicMock(spec=CompanyDataSourceIdentifier)
+def mock_company_data_source():
+    return MagicMock(spec=CompanyDataSource)
 
 
-def test_get_company_data_source_identifiers(mock_db):
+def test_get_company_data_source(mock_db, mock_company_data_source):
     mock_db.query.return_value.filter.return_value.first.return_value = (
-        CompanyDataSource()
+        mock_company_data_source
     )
-    result = get_company_data_source_identifiers(mock_db, 1, 1)
+    result = get_company_data_source(mock_db, 1, 1)
     assert result is not None
 
 
-def test_create_company_data_source_identifier(
-    mock_db, mock_company_data_source_identifier
+def test_get_all_company_data_sources_by_data_source_id(
+    mock_db, mock_company_data_source
 ):
-    identifier_data = IdentifierData(
-        company_data_source_id=1,
-        identifier_key="key",
-        identifier_type=IdentifierType(IdentifierType.AUTOMATICALLY_DISCOVERED),
-        property="property",
-        value="value",
-        validity=datetime.now(),
-    )
+    mock_db.query.return_value.filter.return_value.all.return_value = [
+        mock_company_data_source
+    ]
+    result = get_all_company_data_sources_by_data_source_id(mock_db, 1)
+    assert len(result) == 1
+
+
+def test_get_all_company_data_sources(mock_db, mock_company_data_source):
+    mock_db.query.return_value.all.return_value = [mock_company_data_source]
+    result = get_all_company_data_sources(mock_db)
+    assert len(result) == 1
+
+
+def test_create_company_data_source(mock_db, mock_company_data_source):
+    data = CompanyDataSourceData(1, 1, True, "healthy")
     with patch(
-        "parma_analytics.db.prod.models.company_data_source_identifier.CompanyDataSourceIdentifier",
-        return_value=mock_company_data_source_identifier,
+        "company_data_source_query.CompanyDataSource",
+        return_value=mock_company_data_source,
     ):
-        result = create_company_data_source_identifier(mock_db, identifier_data)
-    assert isinstance(result, CompanyDataSourceIdentifier)
+        result = create_company_data_source(mock_db, data)
+    assert isinstance(result, CompanyDataSource)
 
 
-def test_update_company_data_source_identifier(
-    mock_db, mock_company_data_source_identifier
-):
-    update_data = IdentifierUpdateData(
-        identifier_key="key",
-        identifier_type=IdentifierType(IdentifierType.AUTOMATICALLY_DISCOVERED),
-        property="property",
-        value="value",
-        validity=datetime.now(),
-    )
+def test_update_company_data_source(mock_db, mock_company_data_source):
+    update_data = CompanyDataSourceUpdateData(False, "unhealthy")
     mock_db.query.return_value.filter.return_value.first.return_value = (
-        mock_company_data_source_identifier
+        mock_company_data_source
     )
-    result = update_company_data_source_identifier(mock_db, 1, update_data)
+    result = update_company_data_source(mock_db, 1, update_data)
     assert result is not None
 
 
-def test_delete_company_data_source_identifier(
-    mock_db, mock_company_data_source_identifier
-):
+def test_delete_company_data_source(mock_db, mock_company_data_source):
     mock_db.query.return_value.filter.return_value.first.return_value = (
-        mock_company_data_source_identifier
+        mock_company_data_source
     )
-    result = delete_company_data_source_identifier(mock_db, 1)
+    result = delete_company_data_source(mock_db, 1)
     assert result is True
---
+++ tests/db/test_company_data_source_query.py
@@ -0,0 +1,111 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from parma_analytics.db.prod.company_data_source_query import (
+    CompanyDataSourceData,
+    CompanyDataSourceUpdateData,
+    create_company_data_source,
+    delete_company_data_source,
+    get_all_company_data_sources,
+    get_all_company_data_sources_by_data_source_id,
+    get_company_data_source,
+    update_company_data_source,
+)
+from parma_analytics.db.prod.models.company_data_source import CompanyDataSource
+
+
+# Setup test database and session
+@pytest.fixture
+def mock_db():
+    return MagicMock()
+
+
+@pytest.fixture
+def mock_company_data_source():
+    return MagicMock(spec=CompanyDataSource)
+
+
+def test_get_company_data_source(mock_db):
+    # Setup
+    data = CompanyDataSourceData(1, 1, True, "healthy")
+    create_company_data_source(mock_db, data)
+
+    # Exercise
+    result = get_company_data_source(mock_db, 1, 1)
+
+    # Verify
+    if result is not None:
+        assert result.company_id == 1
+        assert result.data_source_id == 1
+
+
+def test_get_all_company_data_sources_by_data_source_id(mock_db):
+    # Setup
+    res_length = 2
+    data1 = CompanyDataSourceData(1, 1, True, "healthy")
+    data2 = CompanyDataSourceData(1, 2, True, "healthy")
+    create_company_data_source(mock_db, data1)
+    create_company_data_source(mock_db, data2)
+
+    # Exercise
+    result = get_all_company_data_sources_by_data_source_id(mock_db, 1)
+
+    # Verify
+    assert len(result) == res_length
+    for r in result:
+        assert r.data_source_id == 1
+
+
+def test_get_all_company_data_sources(mock_db):
+    # Setup
+    res_length = 2
+    data1 = CompanyDataSourceData(1, 1, True, "healthy")
+    data2 = CompanyDataSourceData(2, 2, True, "healthy")
+    create_company_data_source(mock_db, data1)
+    create_company_data_source(mock_db, data2)
+
+    # Exercise
+    result = get_all_company_data_sources(mock_db)
+
+    # Verify
+    assert len(result) == res_length
+
+
+def test_create_company_data_source(mock_db):
+    # Setup
+    data = CompanyDataSourceData(1, 1, True, "healthy")
+
+    # Exercise
+    result = create_company_data_source(mock_db, data)
+
+    # Verify
+    assert result.company_id == 1
+    assert result.data_source_id == 1
+
+
+def test_update_company_data_source(mock_db):
+    # Setup
+    data = CompanyDataSourceData(1, 1, True, "healthy")
+    create_company_data_source(mock_db, data)
+    update_data = CompanyDataSourceUpdateData(False, "unhealthy")
+
+    # Exercise
+    result = update_company_data_source(mock_db, 1, update_data)
+
+    # Verify
+    if result is not None:
+        assert result.is_data_source_active is False
+        assert result.health_status == "unhealthy"
+
+
+def test_delete_company_data_source(mock_db):
+    # Setup
+    data = CompanyDataSourceData(1, 1, True, "healthy")
+    create_company_data_source(mock_db, data)
+
+    # Exercise
+    result = delete_company_data_source(mock_db, 1)
+
+    # Verify
+    assert result is True
