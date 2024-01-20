dd47af941afb55dc2ce7a9a5971721398eae625a
achref <achref.aloui73@gmail.com>
2024-01-20 13:25:26 +0100

    add test

--- parma_analytics/api/routes/fetch_crm_companies.py
+++ parma_analytics/api/routes/fetch_crm_companies.py
@@ -2,6 +2,8 @@
 from fastapi import APIRouter, status
 from pydantic import BaseModel
 
+from parma_analytics.bl.fetch_crm_companies import get_new_crm_companies_bll
+
 router = APIRouter()
 
 
@@ -21,6 +23,6 @@ class UserId(BaseModel):
 )
 def trigger_fetch_new_companies(user_id: UserId) -> str:
     """Returns the new companies retrieved from the CRM after storing them to the DB."""
-    # new_companies = get_new_crm_companies_bll(user_id)
+    new_companies = get_new_crm_companies_bll(user_id)
 
-    return "successfully fetched"  # new_companies
+    return new_companies
