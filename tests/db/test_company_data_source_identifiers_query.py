82baed3962d68c124bedd85583424621ad7437dd
achref <achref.aloui73@gmail.com>
2024-01-21 00:05:20 +0100

    fix tests

--- tests/db/test_company_data_source_identifiers_query.py
+++ tests/db/test_company_data_source_identifiers_query.py
@@ -1,18 +1,23 @@
+from datetime import datetime
 from unittest.mock import MagicMock, patch
 
 import pytest
 
-from parma_analytics.db.prod.company_data_source_query import (
-    CompanyDataSourceData,
-    CompanyDataSourceUpdateData,
-    create_company_data_source,
-    delete_company_data_source,
-    get_all_company_data_sources,
-    get_all_company_data_sources_by_data_source_id,
-    get_company_data_source,
-    update_company_data_source,
+from parma_analytics.bl.company_data_source_identifiers_bll import (
+    IdentifierData,
+    IdentifierUpdateData,
+)
+from parma_analytics.db.prod.company_data_source_identifiers_query import (
+    create_company_data_source_identifier,
+    delete_company_data_source_identifier,
+    get_company_data_source_identifiers,
+    update_company_data_source_identifier,
 )
 from parma_analytics.db.prod.models.company_data_source import CompanyDataSource
+from parma_analytics.db.prod.models.company_data_source_identifier import (
+    CompanyDataSourceIdentifier,
+    IdentifierType,
+)
 
 
 @pytest.fixture
@@ -21,56 +26,59 @@ def mock_db():
 
 
 @pytest.fixture
-def mock_company_data_source():
-    return MagicMock(spec=CompanyDataSource)
+def mock_company_data_source_identifier():
+    return MagicMock(spec=CompanyDataSourceIdentifier)
 
 
-def test_get_company_data_source(mock_db, mock_company_data_source):
+def test_get_company_data_source_identifiers(mock_db):
     mock_db.query.return_value.filter.return_value.first.return_value = (
-        mock_company_data_source
+        CompanyDataSource()
     )
-    result = get_company_data_source(mock_db, 1, 1)
+    result = get_company_data_source_identifiers(mock_db, 1, 1)
     assert result is not None
 
 
-def test_get_all_company_data_sources_by_data_source_id(
-    mock_db, mock_company_data_source
+def test_create_company_data_source_identifier(
+    mock_db, mock_company_data_source_identifier
 ):
-    mock_db.query.return_value.filter.return_value.all.return_value = [
-        mock_company_data_source
-    ]
-    result = get_all_company_data_sources_by_data_source_id(mock_db, 1)
-    assert len(result) == 1
-
-
-def test_get_all_company_data_sources(mock_db, mock_company_data_source):
-    mock_db.query.return_value.all.return_value = [mock_company_data_source]
-    result = get_all_company_data_sources(mock_db)
-    assert len(result) == 1
-
-
-def test_create_company_data_source(mock_db, mock_company_data_source):
-    data = CompanyDataSourceData(1, 1, True, "healthy")
-    with patch(
-        "company_data_source_query.CompanyDataSource",
-        return_value=mock_company_data_source,
-    ):
-        result = create_company_data_source(mock_db, data)
-    assert isinstance(result, CompanyDataSource)
-
-
-def test_update_company_data_source(mock_db, mock_company_data_source):
-    update_data = CompanyDataSourceUpdateData(False, "unhealthy")
-    mock_db.query.return_value.filter.return_value.first.return_value = (
-        mock_company_data_source
+    identifier_data = IdentifierData(
+        company_data_source_id=1,
+        identifier_key="key",
+        identifier_type=IdentifierType(IdentifierType.AUTOMATICALLY_DISCOVERED),
+        property="property",
+        value="value",
+        validity=datetime.now(),
     )
-    result = update_company_data_source(mock_db, 1, update_data)
+    with patch(
+        "parma_analytics.db.prod.models.company_data_source_identifier.CompanyDataSourceIdentifier",
+        return_value=mock_company_data_source_identifier,
+    ):
+        result = create_company_data_source_identifier(mock_db, identifier_data)
+    assert isinstance(result, CompanyDataSourceIdentifier)
+
+
+def test_update_company_data_source_identifier(
+    mock_db, mock_company_data_source_identifier
+):
+    update_data = IdentifierUpdateData(
+        identifier_key="key",
+        identifier_type=IdentifierType(IdentifierType.AUTOMATICALLY_DISCOVERED),
+        property="property",
+        value="value",
+        validity=datetime.now(),
+    )
+    mock_db.query.return_value.filter.return_value.first.return_value = (
+        mock_company_data_source_identifier
+    )
+    result = update_company_data_source_identifier(mock_db, 1, update_data)
     assert result is not None
 
 
-def test_delete_company_data_source(mock_db, mock_company_data_source):
+def test_delete_company_data_source_identifier(
+    mock_db, mock_company_data_source_identifier
+):
     mock_db.query.return_value.filter.return_value.first.return_value = (
-        mock_company_data_source
+        mock_company_data_source_identifier
     )
-    result = delete_company_data_source(mock_db, 1)
+    result = delete_company_data_source_identifier(mock_db, 1)
     assert result is True
