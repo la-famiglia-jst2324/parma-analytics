a7ed778411ad86bf4e8ae1e8904b69db2160d3e4
onur-oruc <honouroruc@gmail.com>
2024-01-21 20:06:59 +0100

    tests: include other conditions in tests

--- tests/db/test_aggregation_functions.py
+++ tests/db/test_aggregation_functions.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import pytest
-from sqlalchemy import Column, DateTime, Integer, create_engine
+from sqlalchemy import Column, DateTime, Float, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -31,6 +31,24 @@ class MockMeasurementValueType(Base):  # type: ignore
     company_measurement_id = Column(Integer)
     value = Column(Integer)
     timestamp = Column(DateTime)
+
+
+class MockNotificationRule(Base):  # type: ignore
+    """Represents a mock measurement value type.
+
+    Attributes:
+        id (int): The primary key of the measurement value type.
+        company_measurement_id (int): The ID of the company measurement.
+        value (int): The value of the measurement.
+        timestamp (datetime): The timestamp of the measurement.
+    """
+
+    __tablename__ = "mock_notification_rules"
+    rule_id = Column(Integer, primary_key=True)
+    source_measurement_id = Column(Integer)
+    threshold = Column(Float)
+    aggregation_method = Column(String)
+    num_aggregation_entries = Column(Integer)
 
 
 @pytest.fixture
@@ -64,6 +82,75 @@ def test_get_most_recent_measurement_values_no_rule(mock_engine, mock_session):
 
     result = get_most_recent_measurement_values(
         mock_engine, MockMeasurementValueType, timestamp, company_measurement_id
+    )
+
+    # Assert the expected outcome
+    # Adjust the assertion based on the expected result
+    assert result is not None
+    assert result == EXAMPLE_VALUE
+
+
+def test_get_most_recent_values_num_aggregation_entries(mock_engine, mock_session):
+    # Insert mock data into the database
+    mock_data = MockMeasurementValueType(
+        company_measurement_id=EXAMPLE_COMPANY_MEASUREMENT_ID,
+        value=EXAMPLE_VALUE,  # Example value
+        timestamp=datetime.now(),
+    )
+    mock_session.add(mock_data)
+    mock_session.commit()
+
+    mock_notification_rules = MockNotificationRule(
+        source_measurement_id=EXAMPLE_COMPANY_MEASUREMENT_ID,
+        threshold=EXAMPLE_VALUE,
+        aggregation_method="sum",
+        num_aggregation_entries=1,
+    )
+    # Mock parameters
+    timestamp = datetime.now()
+    company_measurement_id = 1
+
+    result = get_most_recent_measurement_values(
+        mock_engine,
+        MockMeasurementValueType,
+        timestamp,
+        company_measurement_id,
+        mock_notification_rules,
+    )
+
+    # Assert the expected outcome
+    # Adjust the assertion based on the expected result
+    assert result is not None
+    assert result == EXAMPLE_VALUE
+
+
+def test_get_most_recent_values_without_num_aggregation_entries(
+    mock_engine, mock_session
+):
+    # Insert mock data into the database
+    mock_data = MockMeasurementValueType(
+        company_measurement_id=EXAMPLE_COMPANY_MEASUREMENT_ID,
+        value=EXAMPLE_VALUE,  # Example value
+        timestamp=datetime.now(),
+    )
+    mock_session.add(mock_data)
+    mock_session.commit()
+
+    mock_notification_rules = MockNotificationRule(
+        source_measurement_id=EXAMPLE_COMPANY_MEASUREMENT_ID,
+        threshold=EXAMPLE_VALUE,
+        aggregation_method="max",
+    )
+    # Mock parameters
+    timestamp = datetime.now()
+    company_measurement_id = 1
+
+    result = get_most_recent_measurement_values(
+        mock_engine,
+        MockMeasurementValueType,
+        timestamp,
+        company_measurement_id,
+        mock_notification_rules,
     )
 
     # Assert the expected outcome
