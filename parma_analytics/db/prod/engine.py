86c2688903ac3f0a2157f237237d94a293153493
Ege Kocabas <kocabas.ege@tum.de>
2024-01-22 00:20:28 +0100

    fix: Use singleton for database engine object for better connection pool

--- parma_analytics/db/prod/engine.py
+++ parma_analytics/db/prod/engine.py
@@ -12,19 +12,23 @@ from sqlalchemy.orm import Session, sessionmaker
 Base = declarative_base()
 
 
+_engine = None
+
+
 def get_engine() -> Engine:
     """Get the database engine."""
-    db_host = quote(os.environ.get("POSTGRES_HOST", "localhost"))
-    db_port = os.environ.get("POSTGRES_PORT", 5432)
-    db_user = quote(os.environ["POSTGRES_USER"])
-    db_password = quote(os.environ["POSTGRES_PASSWORD"])
-    db_name = quote(os.environ.get("POSTGRES_DB", "parma_analytics"))
+    global _engine  # noqa: PLW0603
+    if _engine is None:
+        db_host = quote(os.environ.get("POSTGRES_HOST", "localhost"))
+        db_port = os.environ.get("POSTGRES_PORT", 5432)
+        db_user = quote(os.environ["POSTGRES_USER"])
+        db_password = quote(os.environ["POSTGRES_PASSWORD"])
+        db_name = quote(os.environ.get("POSTGRES_DB", "parma_analytics"))
 
-    db_url = (
-        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-    )
-    engine = create_engine(db_url, client_encoding="utf8")
-    return engine
+        db_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+        _engine = create_engine(db_url, client_encoding="utf8", pool_timeout=40)
+    return _engine
 
 
 @contextmanager
