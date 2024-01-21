c4237df86019fe33ead8de96e0e42edae57ac504
Robin Holzinger <robin.holzinger@tum.de>
2024-01-21 13:56:15 +0100

    Adjust terraform provider versions

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.6"
+      version = "5.12.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
