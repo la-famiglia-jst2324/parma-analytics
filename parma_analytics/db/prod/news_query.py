1e80fcd8644176f64c2899fa38273221a97a3e20
Deepankar Ratra <drattra@gmail.com>
2024-01-25 17:59:21 +0100

    Updating news query to get news of a week

--- parma_analytics/db/prod/news_query.py
+++ parma_analytics/db/prod/news_query.py
@@ -1,5 +1,7 @@
 """Operation related to News."""
 
+from datetime import datetime, timedelta
+
 from sqlalchemy.orm import Session
 
 from parma_analytics.db.prod.models.news import News
@@ -7,4 +9,9 @@ from parma_analytics.db.prod.models.news import News
 
 def get_news_of_company(db: Session, company_id) -> list:
     """Get the news from db."""
-    return db.query(News.message).filter(News.company_id == company_id).all()
+    one_week_ago = datetime.now() - timedelta(weeks=1)
+    return (
+        db.query(News.message)
+        .filter(News.company_id == company_id, News.timestamp >= one_week_ago)
+        .all()
+    )
